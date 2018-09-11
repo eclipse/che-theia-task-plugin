3ac248ec88d1645f953382c33494f3b85d903b6c
Artem Zatsarynnyi <azatsary@redhat.com>
2018-09-11 10:43:32 +0300

    Show already resolved URL of the service in a notification
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- che-theia-task-extension/src/browser/preview/preview-url-notification.ts
+++ che-theia-task-extension/src/browser/preview/preview-url-notification.ts
@@ -12,6 +12,7 @@ import { inject, injectable } from 'inversify';
 import { MessageService } from '@theia/core';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { TaskWatcher, TaskInfo } from '@theia/task/lib/common';
+import { VariableResolverService } from '@theia/variable-resolver/lib/browser';
 import { PREVIEW_ACTION, GO_TO_ACTION } from './previews-widget';
 import { PreviewUrlService } from './preview-url-service';
 import { CheTaskPreferences } from '../task-preferences';
@@ -32,6 +33,9 @@ export class PreviewUrlNotification implements FrontendApplicationContribution {
     @inject(MessageService)
     protected readonly messageService: MessageService;
 
+    @inject(VariableResolverService)
+    protected readonly resolver: VariableResolverService;
+
     onStart(): void {
         this.taskWatcher.onTaskCreated(event => this.onTaskCreated(event));
     }
@@ -48,7 +52,8 @@ export class PreviewUrlNotification implements FrontendApplicationContribution {
         } else if (notify === 'alwaysPreview') {
             this.previewService.preview(cheTask);
         } else {
-            const answer = await this.messageService.info(`Task '${cheTask.label}' launched a service on ${cheTask.previewUrl}`, PREVIEW_ACTION, GO_TO_ACTION);
+            const resolvedURL = await this.resolver.resolve(cheTask.previewUrl!);
+            const answer = await this.messageService.info(`Task '${cheTask.label}' launched a service on ${resolvedURL}`, PREVIEW_ACTION, GO_TO_ACTION);
             if (answer) {
                 this.previewService.preview(cheTask, answer === GO_TO_ACTION);
             }
